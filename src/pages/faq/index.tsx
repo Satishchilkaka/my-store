e28d3678fa6e6c7ae50518f306a75412882e34f3
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-07-25 22:17:34 -0400

    initial commit

---
+++ src/pages/faq/index.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react"
+
+const Faq = () => {
+
+    return  (
+        <Box>
+            <Text> Hello</Text>
+        </Box>
+    )
+}
+
+export default Faq;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Flex, Box, Text } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Flex>
+      <Box>
+        <Text>Home</Text>
+      </Box>
+    </Flex>
+  );
+};
+export default Home;
