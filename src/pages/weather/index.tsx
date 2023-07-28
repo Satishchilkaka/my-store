31d08abd2f88140dc969c35f70d9c9903321b58d
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-07-27 23:22:24 -0400

    update

--- src/pages/weather/index.tsx
+++ src/pages/weather/index.tsx
@@ -1,12 +1,44 @@
-import { Box } from "@chakra-ui/react"
 
-const Weather = () => {
+import { useEffect, useState } from 'react';
+import { fetchWeatherData } from '../../util/weatherAPI';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 
-    return (
-        <Box>
-            Hello
-        </Box>
-    )
+interface WeatherData {
+  location: {
+    name: string;
+    country: string;
+  };
+  current: {
+    temp_c: number;
+    condition: {
+      text: string;
+    };
+  };
 }
 
-export default Weather;
+const WeatherPage = () => {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+
+  useEffect(() => {
+    // Replace 'Toronto' with the desired city name
+    fetchWeatherData('Toronto')
+      .then((data) => setWeatherData(data))
+      .catch((error) => console.error('Error fetching weather data:', error));
+  }, []);
+
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Flex direction="column" align="center" justify="center" h="100vh">
+      <Heading as="h1" size="xl" mb={4}>
+        Current Weather in {weatherData.location.name}, {weatherData.location.country}
+      </Heading>
+      <Text fontSize="xl">Temperature: {weatherData.current.temp_c}°C</Text>
+      <Text fontSize="xl">Condition: {weatherData.current.condition.text}</Text>
+    </Flex>
+  );
+};
+
+export default WeatherPage;
