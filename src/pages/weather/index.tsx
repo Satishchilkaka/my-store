3f1d2c623e187f4b0f9ffed1578529baed16ce7c
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-08-10 15:23:41 -0400

    add current weather report

--- src/pages/weather/index.tsx
+++ src/pages/weather/index.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from 'react';
-import { fetchWeatherData } from '../../util/weatherAPI';
-import { Flex, Heading, Text } from '@chakra-ui/react';
+import React, { useState } from 'react'; // Import useState
+import { fetchWeatherData } from '../../util/currentWeather';
+import { Box, Button, Flex, FormControl, Heading, Input, Text } from '@chakra-ui/react';
 
 interface WeatherData {
   location: {
     name: string;
     country: string;
+    localtime: string;
   };
   current: {
     temp_c: number;
@@ -17,25 +18,57 @@ interface WeatherData {
 
 const WeatherPage = () => {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [cityName, setCityName] = useState<string>(''); // State to store the entered city name
 
-  useEffect(() => {
-    // Replace 'Toronto' with the desired city name
-    fetchWeatherData('Toronto')
+  const handleGetWeather = () => {
+    fetchWeatherData(cityName) 
       .then((data) => setWeatherData(data))
       .catch((error) => console.error('Error fetching weather data:', error));
-  }, []);
+  };
 
-  if (!weatherData) {
-    return <div>Loading...</div>;
+  const isValidInput = () => {
+    return (cityName !== '')
+      
+    
   }
 
   return (
-    <Flex direction="column" align="center" justify="center" h="100vh">
-      <Heading as="h1" size="xl" mb={4}>
-        Current Weather in {weatherData.location.name}, {weatherData.location.country}
-      </Heading>
-      <Text fontSize="xl">Temperature: {weatherData.current.temp_c}°C</Text>
-      <Text fontSize="xl">Condition: {weatherData.current.condition.text}</Text>
+    <Flex alignContent={'center'} ml={'100px'} mt={'70px'}>
+      <Heading>current Weather</Heading>
+      <Box>
+        <FormControl>
+          <label>Enter your city name:</label>
+          <Input
+            type='text'
+            name='cityName'
+            placeholder='Enter city name'
+            mt={1}
+            borderRadius={'10px'}
+            borderColor={'#FFFFF'}
+            maxW={'250px'}
+            value={cityName} 
+            onChange={(e) => setCityName(e.target.value)} 
+            
+          />
+        </FormControl>
+        <Box mt={3} >
+          <Button bg={'#4391F2'} onClick={handleGetWeather}  isDisabled = {!isValidInput()}>
+            Get Weather Data
+          </Button>
+        </Box>
+      </Box>
+
+      {/* Display weather data if available */}
+      {weatherData && (
+        <Box mt={4}>
+          <Heading as="h2" size="md">
+            Current Weather in {weatherData.location.name}, {weatherData.location.country}
+          </Heading>
+          <Text fontSize="md">Temperature: {weatherData.current.temp_c}°C</Text>
+          <Text fontSize="md">Condition: {weatherData.current.condition.text}</Text>
+          <Text fontSize="md">Local time: {weatherData.location.localtime}</Text>
+        </Box>
+      )}
     </Flex>
   );
 };
--- src/util/currentWeather.tsx
+++ src/util/currentWeather.tsx
@@ -1,5 +1,5 @@
 
-const API_KEY = process.env.WEATHER_API_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
 console.log('api key: ' + JSON.stringify(API_KEY))
 
@@ -9,7 +9,7 @@ export async function fetchWeatherData(city: string) {
   }
 
   const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
+    `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`
   );
   const data = await response.json();
   return data;
