f05859e8f77e465bb2d2e8e5ebe69b33b8c1b3e8
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-08-18 18:12:05 -0400

    update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { Flex, IconButton, Switch, useColorMode, Text,Link as ChakraLink } from '@chakra-ui/react';
-import { FiSun, FiMoon } from 'react-icons/fi';
-import { useRouter } from 'next/router';
+import React from "react";
+import {
+  Flex,
+  IconButton,
+  Switch,
+  useColorMode,
+  Text,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import { FiSun, FiMoon } from "react-icons/fi";
+import { useRouter } from "next/router";
 
 type NavigationItem = {
   label: string;
@@ -10,16 +17,16 @@ type NavigationItem = {
 
 const paths: NavigationItem[] = [
   {
-    label: 'Home',
-    path: '/',
+    label: "Home",
+    path: "/",
   },
   {
-    label: 'Current Weather',
-    path: '/current',
+    label: "Current Weather",
+    path: "/current",
   },
   {
-    label: 'Forecast Weather',
-    path: '/forecast',
+    label: "Forecast Weather",
+    path: "/forecast",
   },
 ];
 
@@ -27,35 +34,62 @@ export const Header: React.FC = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   const router = useRouter();
 
+  const getSelectedPath = () => { 
+    const route = router.pathname 
+    return route
+
+  }
+const currentPath = getSelectedPath();
+
   const handlePathChange = (path: string) => {
     if (path !== router.pathname) {
       router.push(path);
     }
   };
 
+  console.log('path', currentPath)
   return (
     <Flex justifyContent="space-between" alignItems="center" p={4} bg="#CCCCCC">
-      <Flex alignItems="center" gap={4}>
+      <Flex alignItems="end" gap={1}>
         {paths.map((item) => (
-          <Flex alignItems={'center'}
-          w={200}
-          h={50}
-          ml={1}
-mr={1}
+          <Flex
+            alignItems={"center"}
+            
+            minWidth={200}
+            h={14}
+            mr={1}
+            ml={1}
+            border={'md'}
+            rounded={'md'}
             key={item.path}
             fontSize="md"
             onClick={() => handlePathChange(item.path)}
-            color={item.path === router.pathname ? '#000000' : 'inherit'}
+            color={item.path === router.pathname ? "black" : "inherit"}
             cursor="pointer"
-            _hover={{ bg: '#292929',
-          color: 'white' }}
+            _hover={{ bg: "#292929", color: "white" }}
+            _dark={{
+              bg: "#999999",
+              _hover: {
+                bg: "#000000",
+              },
+            }}
+            
+bg={
+  currentPath == item.path ?
+  '#666666' : 'grey'
+}
+
+
           >
-          <Text fontSize={'20px'} textTransform={'uppercase'}> {item.label}</Text>  
+            <Text fontSize={"20px"} textTransform={"uppercase"} ml={3} mr={2}>
+              {" "}
+              {item.label}
+            </Text>
           </Flex>
         ))}
         <IconButton
           aria-label="Toggle Theme"
-          icon={colorMode === 'dark' ? <FiSun /> : <FiMoon />}
+          icon={colorMode === "dark" ? <FiSun /> : <FiMoon />}
           onClick={toggleColorMode}
         />
       </Flex>
