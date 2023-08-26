b61d1eb31236497b9a3f8bdaa19be1d3c6e3693d
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-08-26 17:35:23 -0400

    update sign in form

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   FormControl,
   FormLabel,
@@ -7,115 +7,117 @@ import {
   Alert,
   AlertIcon,
   Checkbox,
-} from '@chakra-ui/react';
-import axios from 'axios';
-import { useSession } from 'next-auth/react';
-
-import { useRouter } from 'next/router'; 
+  Box,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useRouter } from "next/router";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-
 import * as Yup from "yup";
+import React from "react";
+import { useFormik } from "formik";
 
 const validationSchema = Yup.object({
   username: Yup.string().required("Username is required"),
   password: Yup.string().required("Password is required"),
 });
 
-
 export const SignInForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const router = useRouter(); // Initialize the useRouter hook
+  const [errorMessage, setErrorMessage] = useState("");
+  const router = useRouter();
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
+  const handleSubmit = async (values: { username: any; password: any }) => {
     try {
-      const response = await axios.post('http://localhost:3001/v1/login', {
-        username,
-        password,
+      const response = await axios.post("http://localhost:3001/v1/login", {
+        username: values.username,
+        password: values.password,
       });
 
       console.log(response.data.message);
-      setErrorMessage('');
+      setErrorMessage("");
       const token = response.data.token;
-      localStorage.setItem('token', token);
+      localStorage.setItem("token", token);
 
-console.log('get token and set token', token);
-      router.push('/current'); 
+      console.log("get token and set token", token);
+      router.push("/current");
     } catch (error) {
-      setErrorMessage('Invalid credentials');
+      setErrorMessage("Invalid credentials");
     }
   };
-const msg = 'Required'
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
+
   return (
-    <form onSubmit={handleSubmit}>
-    <FormControl mb={6}>
-      <FormLabel>Username</FormLabel>
+<form onSubmit={formik.handleSubmit}>
+  <FormControl mb={10} h="68px">
+    <FormLabel>Username</FormLabel>
+    <div style={{ width: "100%" }}>
       <Input
         type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        name="username"
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        isInvalid={formik.touched.username && !!formik.errors.username}
+        width="100%"
       />
-      {errorMessage && (
-        <Alert status="error" fontSize="12px" bg="none">
-          <AlertIcon />
-          {errorMessage}
-        </Alert>
-      )}
-    </FormControl>
-    <FormControl mb={6} mt={5}>
-      <FormLabel>Password</FormLabel>
+      {formik.touched.username && formik.errors.username ? (
+        <Box color="red" fontSize="small" marginTop="4px">
+          {formik.errors.username}
+        </Box>
+      ) : null}
+    </div>
+  </FormControl>
+  <FormControl mb={10} mt={5} h="68px">
+    <FormLabel>Password</FormLabel>
+    <div style={{ width: "100%" }}>
       <Input
         type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        name="password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        isInvalid={formik.touched.password && !!formik.errors.password}
+        width="100%"
       />
-      {errorMessage && (
-        <Alert status="error" fontSize="12px" bg="none">
+      {formik.touched.password && formik.errors.password ? (
+        <Box color="red" fontSize="small" marginTop="4px">
+          {formik.errors.password}
+        </Box>
+      ) : null}
+    </div>
+  </FormControl>
+
+
+
+  <FormControl mb={2} >
+    <Checkbox name="rememberMe">Remember Me</Checkbox>
+  </FormControl>
+
+  <Button type="submit" size="lg" mt={3} mb={5} width={'70%'}>
+    Sign In
+  </Button>
+
+  {errorMessage && (
+        <Alert status="error" >
           <AlertIcon />
           {errorMessage}
         </Alert>
       )}
-    </FormControl>
-    <FormControl mb={4}>
-      <Checkbox name="rememberMe">Remember Me</Checkbox>
-    </FormControl>
-    <Button type="submit" size="lg">
-      Login
-    </Button>
-  </form>
+  {/* {errorMessage ? (
+    <Box color="red" fontSize="small" marginTop="4px">
+      {errorMessage}
+    </Box>
+  ) : null} */}
   
+</form>
 
-    // <Formik
-    //   initialValues={{ username: "", password: "", rememberMe: false }}
-    //   validationSchema={validationSchema}
-    //   onSubmit={handleSubmit}
-    // >
-    //   <Form>
-    //     <FormControl>
-    //       <FormLabel>Username</FormLabel>
-    //       <Field as={Input} type="text" name="username" />
-    //       <ErrorMessage name="username">
-    //         {(msg: string) => <Alert status="error">{msg}</Alert>}
-    //       </ErrorMessage>
-    //     </FormControl>
-    //     <FormControl mt={4}>
-    //       <FormLabel>Password</FormLabel>
-    //       <Field as={Input} type="password" name="password" />
-    //       <ErrorMessage name="password">
-    //         {(msg: string) => <Alert status="error">{msg}</Alert>}
-    //       </ErrorMessage>
-    //     </FormControl>
-    //     <FormControl mt={16}>
-    //       <Checkbox name="rememberMe">Remember Me</Checkbox>
-    //     </FormControl>
-    //     <Button type="submit" mt={4}>
-    //       Login
-    //     </Button>
-    //   </Form>
-    // </Formik>
+
   );
 };
-
