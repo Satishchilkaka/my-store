2e1765f26147ebae8ec45f909d798b0fec1d2624
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-09-21 23:44:27 -0400

    update

--- src/util/awsConfig.ts
+++ src/util/awsConfig.ts
@@ -1,4 +1,7 @@
 import AWS from 'aws-sdk';
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const region = process.env.AWS_REGION
 
 AWS.config.update({
   accessKeyId: 'your-access-key-id',
