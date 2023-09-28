dc86fa8da9c95cdd62fd97802d329e8d59c6ecad
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-09-28 19:31:47 -0400

    update config

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -5,20 +5,20 @@ import { s3, bucketName, region } from '../../util/awsConfig';
 const FAQ = () => {
   console.log('aws', s3);
 
-  const listObjectsParams = {
-    Bucket: bucketName, 
-  };
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  const region = process.env.AWS_REGION;
+  const bucketName = process.env.AWS_BUCKET_NAME;
+  
 
-  s3.listObjects(listObjectsParams, (err, data) => {
-    if (err) {
-      console.error('Error listing objects:', err);
-    } else {
-      data.Contents.forEach((object) => {
-        const imageUrl = `https://${bucketName}.s3.${region}.amazonaws.com/`;
-        console.log('Image URL:', imageUrl);
-      });
-    }
-  });
+  
+  const config = {
+      bucketName: bucketName,
+      region: region,
+      accessKeyId: accessKeyId,
+      secretAccessKey: secretAccessKey
+  }
+  console.log(config)
 
   return (
     <Layout title="Products" noHeader={false} withNoMenus={true}>
