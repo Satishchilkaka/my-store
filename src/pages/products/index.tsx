da680f77e6eaed631bcd497b33dc0367754ef801
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-10-05 18:36:54 -0400

    update logic

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -19,6 +19,7 @@ import {
   NumberIncrementStepper,
   NumberDecrementStepper,
   Button,
+  Spinner, // Import Spinner from Chakra UI
 } from "@chakra-ui/react";
 import { Layout } from "@/components/Layout";
 
@@ -35,19 +36,23 @@ const ProductList = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true); // State to manage loading state
   const api = process.env.NEXT_PUBLIC_API_URL;
+
   useEffect(() => {
     async function fetchProducts() {
       try {
         const response = await axios.get(`${api}/v1/products`, {});
         setProducts(response.data);
+        setIsLoading(false); // Set loading to false when data is fetched
       } catch (error) {
         console.error("Error fetching products:", error);
+        setIsLoading(false); // Set loading to false in case of an error
       }
     }
 
     fetchProducts();
-  }, []);
+  }, [api]);
 
   const handleQuantityChange = (productId: string, newQuantity: string) => {
     const parsedQuantity = parseInt(newQuantity, 10);
@@ -65,6 +70,11 @@ const ProductList = () => {
 
   return (
     <Layout title="Products" noHeader={false} withNoMenus={true}>
+        {isLoading ? (
+          <Spinner size="xl" color="teal" />
+        ) : (
+
+          
       <Box p="4">
         <Input
           type="text"
@@ -110,78 +120,72 @@ const ProductList = () => {
           <TabPanels></TabPanels>
         </Tabs>
 
-        <Grid templateColumns="repeat(auto-fill, minmax(245px, 1fr))" gap={3}>
-  {products
-    .filter((product) =>
-      selectedCategory && selectedCategory !== "All"
-        ? product.category === selectedCategory
-        : true
-    )
-    .filter((product) =>
-      searchQuery && typeof searchQuery === "string" && product.name
-        ? product.name.toLowerCase().includes(searchQuery.toLowerCase())
-        : true
-    )
-    .map((product) => (
-      <GridItem
-        key={product._id}
-        p={4}
-        borderRadius="md"
-        boxShadow="md"
-      >
-        <Flex alignItems="center">
-          <Text fontSize="lg" fontWeight="bold" mb={2}>
-            {product.name}
-          </Text>
-        </Flex>
-        <Image
-          src={product.imageURL}
-          alt={product.name}
-          maxH="150px"
-          objectFit="cover"
-        />
-        <Flex alignItems="center">
-          <Text fontSize="md" fontWeight="bold">
-            Price: ${product.price}
-          </Text>
-        </Flex>
-        <Flex display={"flex"} alignItems="center" gap={2}>
-          <Text fontSize="md">Quantity:</Text>
-          <NumberInput
-            minWidth="60px"
-            maxWidth="80px"
-            defaultValue={1}
-            size="sm"
-            min={1}
-            onChange={(newQuantity) =>
-              handleQuantityChange(product._id, newQuantity)
-            }
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </Flex>
-        <Button mt={2} colorScheme="teal">
-          Add to cart
-        </Button>
-      </GridItem>
-    ))}
-  {(!products.length || products.length === 0) && (
-    <Text alignContent={"center"} fontSize="lg" fontWeight="bold" mt={4}>
-      No products found.
-    </Text>
-  )}
-</Grid>
-
+      
+          <Grid templateColumns="repeat(auto-fill, minmax(245px, 1fr))" gap={3}>
+            {products
+              .filter((product) =>
+                selectedCategory && selectedCategory !== "All"
+                  ? product.category === selectedCategory
+                  : true
+              )
+              .filter((product) =>
+                searchQuery && typeof searchQuery === "string" && product.name
+                  ? product.name.toLowerCase().includes(searchQuery.toLowerCase())
+                  : true
+              )
+              .map((product) => (
+                <GridItem
+                  key={product._id}
+                  p={4}
+                  borderRadius="md"
+                  boxShadow="md"
+                >
+                  <Flex alignItems="center">
+                    <Text fontSize="lg" fontWeight="bold" mb={2}>
+                      {product.name}
+                    </Text>
+                  </Flex>
+                  <Image
+                    src={product.imageURL}
+                    alt={product.name}
+                    maxH="150px"
+                    objectFit="cover"
+                  />
+                  <Flex alignItems="center">
+                    <Text fontSize="md" fontWeight="bold">
+                      Price: ${product.price}
+                    </Text>
+                  </Flex>
+                  <Flex display={"flex"} alignItems="center" gap={2}>
+                    <Text fontSize="md">Quantity:</Text>
+                    <NumberInput
+                      minWidth="60px"
+                      maxWidth="80px"
+                      defaultValue={1}
+                      size="sm"
+                      min={1}
+                      onChange={(newQuantity) =>
+                        handleQuantityChange(product._id, newQuantity)
+                      }
+                    >
+                      <NumberInputField />
+                      <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </NumberInput>
+                  </Flex>
+                  <Button mt={2} colorScheme="teal">
+                    Add to cart
+                  </Button>
+                </GridItem>
+              ))}
+          </Grid>
+       
       </Box>
+      )}
     </Layout>
   );
 };
 
 export default ProductList;
-
-
-// Fix a bug with all tab search
