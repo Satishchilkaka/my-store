4f512d0940793a82fac226bf9d935c78feb2fae1
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-10-10 23:05:50 -0400

    update cart page

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,25 +1,60 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Button, Image } from "@chakra-ui/react";
 import { Layout } from "@/components/Layout";
 import { useCart } from "@/util/cartFunction";
 
 const Cart = () => {
+  const { cart, addToCart } = useCart();
 
-    const { cart, addToCart } = useCart();
-const item = cart[0].name
+  // Function to remove a product from the cart
+  const removeFromCart = (productId: string) => {
+    const updatedCart = cart.filter((product) => product._id !== productId);
+  };
+
+  // Function to proceed to checkout
+  const checkout = () => {
+    // Implement the logic to proceed to checkout
+  };
 
   return (
     <Layout title="Cart" noHeader={false} withNoMenus={true}>
+      <Box p={4}>
+        <Text fontSize="xl" fontWeight="bold">
+          Shopping Cart
+        </Text>
 
-    <Box p={4}>
-      <Text fontSize="xl" fontWeight="bold">
-        Shopping Cart
-      </Text>
-      <Text>{item}</Text>
-    </Box>
+        <Box mt={4}>
+          {cart.map((product) => (
+            <Box
+              key={product._id}
+              border="1px solid #ccc"
+              p={3}
+              borderRadius="md"
+              mb={4}
+            >
+              <Text fontSize="lg" fontWeight="bold">
+                {product.name}
+              </Text>
+              <Image src={product.imageURL} alt={product.name} maxH="150px" objectFit="cover" mb={2} />
+              <Text fontSize="md">Price: ${product.price}</Text>
+              <Text fontSize="md">Quantity: {product.quantity}</Text>
+              <Button
+                colorScheme="red"
+                size="sm"
+                onClick={() => removeFromCart(product._id)}
+              >
+                Remove
+              </Button>
+            </Box>
+          ))}
+        </Box>
+
+        <Button colorScheme="teal" mt={4} onClick={checkout}>
+          Proceed to Checkout
+        </Button>
+      </Box>
     </Layout>
   );
-
 };
 
 export default Cart;
