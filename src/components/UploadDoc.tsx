aa92f7e6dc309ee610f05c067f875e31feceec14
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-10-31 20:06:20 -0400

    fix bug with upload document

--- src/components/UploadDoc.tsx
+++ src/components/UploadDoc.tsx
@@ -1,39 +1,89 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Box, Button, Center } from "@chakra-ui/react";
+import { Box, Button, Input, Center, useToast } from "@chakra-ui/react";
 
 export const UploadDoc: React.FC = () => {
-  const [file, setFile] = useState<File | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0];
-    if (selectedFile) {
-      setFile(selectedFile);
+  const validateSelectedFile = (file: File) => {
+    return new Promise<void>((resolve, reject) => {
+      if (!file) {
+        reject(new Error("No file selected."));
+      } else {
+        const allowedExtensions = [".pdf", ".jpeg", ".jpg", ".png", ".doc", ".docx"];
+        const extname = file.name.slice(((file.name.lastIndexOf(".") - 1) >>> 0) + 2).toLowerCase();
+
+        if (allowedExtensions.includes(extname)) {
+          resolve();
+        } else {
+          reject(new Error("Invalid file type."));
+        }
+      }
+    });
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0] || null;
+    setSelectedFile(file);
+
+    if (file) {
+      setIsLoading(true);
+      validateSelectedFile(file)
+        .then(() => setIsLoading(false))
+        .catch((error) => {
+          setIsLoading(false);
+          console.error(error);
+        });
     }
   };
 
   const handleUpload = async () => {
-    if (file) {
+    if (selectedFile) {
       const formData = new FormData();
-      formData.append("document", file);
-      
+      formData.append("file", selectedFile);
+
       try {
-        await axios.post("http://localhost:3001/v1/upload-document/", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        });
-        alert("Document uploaded successfully!");
+        const uploadResponse = await axios.post("http://localhost:3001/v1/upload-document/", formData);
+
+        if (uploadResponse.status === 200) {
+          toast({
+            title: "File Upload Successful",
+            description: "The file has been successfully uploaded.",
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+          });
+          setSelectedFile(null);
+        } else {
+          console.error("File upload failed:", uploadResponse.statusText);
+        }
       } catch (error) {
-        console.error("Error uploading document:", error);
+        console.error("File upload failed:", error);
+        toast({
+          title: "File Upload Failed",
+          description: "There was an error while uploading the file. Please re-try.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
       }
+    } else {
+      console.error("No file selected.");
     }
   };
 
   return (
+   
       <Box>
-        <input type="file" accept=".pdf, .jpeg, .jpg, .png, .doc, .docx" onChange={handleFileChange} />
+        <Input
+          type="file"
+          accept=".pdf, .jpeg, .jpg, .png, .doc, .docx, .txt, .zip"
+          onChange={handleFileChange}
+        />
         <Button onClick={handleUpload}>Upload Document</Button>
       </Box>
+   
   );
 };
